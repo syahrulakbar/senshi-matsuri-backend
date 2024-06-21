fceedb0d581dee4df2d5748cb96099e1dcd889a9
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-06-21 11:51:29 +0700

    add new link production

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const corsOPTIONS = {
-  origin: ["http://localhost:3000", "https://senshi-matsuri.vercel.app"],
+  origin: [
+    "http://localhost:3000",
+    "https://senshi-matsuri.vercel.app",
+    "https://senshi-matsuri-x.vercel.app",
+  ],
   credentials: true,
 };
 
